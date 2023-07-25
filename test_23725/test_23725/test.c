72fd0d32e2bab9218764291ce8b016bd06c9e8de
Racard0 <dongyan2580@qq.com>
2023-07-25 17:06:29 +0800

    23725

---
+++ test_23725/test_23725/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+  int num1,num2;
+  scanf("%d %d",&num1,& num2);
+  if (num1 > num2)
+  {
+    printf("%d",num1);
+  }
+  else
+  {
+    printf("%d",num2);
+  }
+  return 0;
+}
